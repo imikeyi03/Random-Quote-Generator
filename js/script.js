1528dcb949078c303f60d36ed036ddf0ba737cc9
Mikey Mann <mikey.mannx3@gmail.com>
2020-02-28 14:10:46 -0800

    Added all quotes

--- js/script.js
+++ js/script.js
@@ -9,9 +9,59 @@ project 1 - A Random Quote Generator
 
   let quotes = [
     {
-        
+      "quote" : "Don’t downgrade your dream just to fit your reality, upgrade your conviction to match your destiny.",
+      "source" : "Stuart Scott"  
+    },
+    {
+      "quote" : "You are braver than you believe, stronger than you seem and smarter than you think.",
+      "source" : "Disney",
+      "citation" : "Winnie the Pooh"
+    },
+    {
+      "quote" : "You are confined only by the walls you build yourself.",
+      "source" : "Andrew Murphy"
+    },
+    {
+      "quote" : "It's the possibility of having a dream come true that makes life interesting.",
+      "source" : "Paulo Coelho",
+      "citation" : "The Alchemist",
+      "year" : 1988
+    },
+    {
+      "quote" : "It was times like these when I thought my father, who hated guns and had never been to any wars, was the bravest man who ever lived.",
+      "source" : "Harper Lee",
+      "citation" : "To Kill A Mockingbird",
+      "year" : 1987
+    },
+    {
+      "quote" : "Do I love you? My God, if your love were a grain of sand, mine would be a universe of beaches.",
+      "source" : "William Goldman",
+      "citation" : "The Princess Bride"
+    },
+    {
+      "quote" : "Good morning, Vietnam!",
+      "source" : "Adrian Cronauer",
+      "citation" : "Good morning, Vietnam",
+      "year" : 1987
+    },
+    {
+      "quote" : "The Dude abides.",
+      "source" : "The Big Lebowski",
+      "citation" : "The Dude",
+      "year" : 1998
+    },
+    {
+      "quote" : "I'm not bad. I'm just drawn that way.",
+      "source" : "Who Framed Roger Rabbit",
+      "citation" : "Jessica Rabit",
+      "year" : 1988
+    },
+    {
+      "quote" : "You're gonna need a bigger boat.",
+      "source" : "Jaws",
+      "citation" : "Martin Brody",
+      "year" :  1975
     }
-
 ];
 
 
